5444c4c069e10acda74e9ed0dd1326386f141867
Karsten Rabe <karstenrabe91@gmail.com>
2019-05-16 23:52:04 -0400

    Add buggy parallax effect

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 
 // Component imports 
 import Header from '../../Components/Header';
+import ParallaxImageSection from '../../Components/ParallaxImageSection';
 
 // Page theming
 import themes from '../../themes';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 const Landing = () => {
-    return (
-        <MuiThemeProvider theme={themes.CoolDarkBlue}>
-            <Header />
-        </MuiThemeProvider>
-    );
-}
+	return (
+		<MuiThemeProvider theme={themes.CoolDarkBlue}>
+			<Header />
+			<ParallaxImageSection />
+		</MuiThemeProvider>
+	);
+};
 
 export default Landing;
