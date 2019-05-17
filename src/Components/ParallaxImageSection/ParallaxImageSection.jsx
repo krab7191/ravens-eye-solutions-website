21ebfb428dddb8a6dfa03b86b4f2ca0aaf5de6f2
Karsten Rabe <karstenrabe91@gmail.com>
2019-05-17 00:46:47 -0400

    Add stable parallax and initial website text

--- src/Components/ParallaxImageSection/ParallaxImageSection.jsx
+++ src/Components/ParallaxImageSection/ParallaxImageSection.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 // Dependency imports
+import Typography from '@material-ui/core/Typography';
 
 // Style imports 
 import './ParallaxImageSection.css';
@@ -9,15 +10,14 @@ import './ParallaxImageSection.css';
 
 const ParallaxImageSection = () => (
     <>
-        <div className="parallax-wrapper section">
-        	<div className="parallax-content">
-                Intelligentsia asymmetrical pour-over sriracha hoodie mumblecore Helvetica you probably haven't heard of them keytar 8-bit Cosby sweater salvia forage DIY retro synth semiotics direct trade  3 wolf moon tote bag pork belly wolf tofu messenger bag deep v try-hard gentrify normcore before they sold out flexitarian irony Etsy XOXO drinking vinegar next level tousled post-ironic Echo Park seitan Carles master cleanse polaroid bitters actually trust fund sartorial distillery lo-fi Thundercats scenester Neutra chillwave Tonx Schlitz dreamcatcher chia cray ennui organic Truffaut fap iPhone fanny pack selfies viral vegan shabby chic Tumblr Austin Pitchfork bespoke aesthetic cardigan brunch Banksy twee High Life PBR crucifix ethical ugh photo booth tattooed stumptown paleo sustainable typewriter readymade slow-carb selvage Wes Anderson freegan swag chambray meggings keffiyeh cliche small batch fashion axe artisan cred put a bird on it McSweeney's vinyl disrupt kitsch hella roof party bicycle rights Brooklyn mixtape single-origin coffee farm-to-table quinoa gluten-free art party +1 pop-up Kickstarter craft beer YOLO meh locavore narwhal pug hashtag church-key cornhole American Apparel leggings Bushwick blog wayfarers beard kale chips umami kogi street art four loko lomo Odd Future yr skateboard whatever Pinterest literally flannel occupy gastropub Shoreditch Marfa Williamsburg raw denim butcher Vice PBR&B jean shorts mustache authentic squid biodiesel banjo mlkshk heirloom letterpress Blue Bottle pickled VHS Portland food truck plaid fixie fingerstache Godard 90's banh mi
-        	</div>
-        </div>
-        <div class="regular-wrapper">
-        	<div class="normal-content">
-        		90's iPhone freegan bespoke sustainable Shoreditch literally viral banjo put a bird on it master cleanse shabby chic retro narwhal Portland dreamcatcher Echo Park try-hard before they sold out kogi small batch 3 wolf moon Etsy keytar Kickstarter squid skateboard scenester hella brunch tote bag polaroid flexitarian cardigan YOLO drinking vinegar tattooed locavore raw denim direct trade  cray Thundercats mlkshk pickled asymmetrical paleo keffiyeh leggings lo-fi crucifix Odd Future messenger bag artisan cornhole banh mi High Life synth heirloom pork belly sriracha craft beer selfies Neutra VHS Marfa flannel cliche trust fund blog jean shorts ennui Blue Bottle farm-to-table Tumblr hashtag forage chillwave you probably haven't heard of them church-key Truffaut fanny pack chambray wayfarers gastropub Austin salvia Brooklyn readymade Helvetica ethical Vice Bushwick Schlitz Tonx kale chips gluten-free selvage aesthetic roof party Pinterest hoodie tousled disrupt mumblecore organic twee vinyl meh XOXO American Apparel McSweeney's plaid umami mustache quinoa post-ironic tofu Wes Anderson pour-over four loko typewriter whatever fixie beard fap mixtape cred Intelligentsia yr Banksy ugh occupy vegan slow-carb irony biodiesel street art DIY actually fashion axe stumptown single-origin coffee fingerstache next level pop-up Pitchfork 8-bit kitsch swag gentrify art party meggings normcore PBR&B sartorial seitan Godard chia +1 lomo semiotics butcher Cosby sweater wolf photo booth deep v bicycle rights food truck letterpress authentic Williamsburg bitters Carles PBR pug distillery
-        	</div>
+        <div className="parallax-container">
+        	<Typography
+        		variant="h2"
+        		align="center"
+        		color="inherit"
+        		className="parallax-text">
+                We see the future of your business
+          	</Typography>
         </div>
     </>
 );
